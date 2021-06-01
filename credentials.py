9ac445ff842bd64dcc3828b019a6cc5eb7cb9770
midigo0411 <midigo041@gmail.com>
2021-06-01 23:18:19 +0300

    methods that checks if the name password enterered match entries

--- credentials.py
+++ credentials.py
@@ -2,3 +2,17 @@ class Credential:
     '''
     class to create account credentials, generate passwords and save their information
     '''
+    # creating a class Variables
+    credentials_list = []
+    user_credentials_list = []
+
+    @classmethod
+    def check_user(hls, first_name,password):
+        '''
+        Method that checks if the name and password entered match entries in the users_list
+		'''
+        current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
