a858b323e68265dae4af4b12eccd1006d757d908
midigo0411 <midigo041@gmail.com>
2021-06-01 23:23:49 +0300

    function to save newly created user instance

--- credentials.py
+++ credentials.py
@@ -16,3 +16,16 @@ class Credential:
 			if (user.first_name == first_name and user.password == password):
 				current_user = user.first_name
 		return current_user
+
+    def __init__(self,user_name,site_name,account_name,password):
+        '''
+		Method to define the properties for each user object will hold.
+		'''
+        # instance variables
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
+
+        # global users_list
+		Credential.credentials_list.append(self)
