a42263409e5a8dc788d12a68b251e4af8fdc8188
midigo0411 <midigo041@gmail.com>
2021-06-02 00:36:17 +0300

    add test

--- password-locker.py
+++ password-locker.py
@@ -1,7 +1,5 @@
-import pyperclip
-from credentials import Credential
+import random
 from user import User
-
 
 def create_user(fname,lname,password):
 	'''
@@ -10,67 +8,112 @@ def create_user(fname,lname,password):
 	new_user = User(fname,lname,password)
 	return new_user
 
-    def save_user(user):
+def save_user(user):
 	'''
 	Function to save a new user account
 	'''
 	User.save_user(user)
 
-    def verify_user(first_name,password):
+
+def verify_user(first_name,password):
 	'''
 	Function that verifies the existance of the user before creating credentials
 	'''
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
 
-    def generate_password():
+def generate_password():
 	'''
 	Function to generate a password automatically
 	'''
 	gen_pass = Credential.generate_password()
 	return gen_pass
 
-    def create_credential(user_name,site_name,account_name,password):
+def create_credential(user_name,site_name,account_name,password):
 	'''
 	Function to create a new credential
 	'''
 	new_credential=Credential(user_name,site_name,account_name,password)
 	return new_credential
 
-    def save_credential(credential):
+def save_credential(credential):
 	'''
 	Function to save a newly created credential
 	'''
 	Credential.save_credentials(credential)
 
-
-    def display_credentials(user_name):
+def display_credentials(user_name):
 	'''
 	Function to display credentials saved by a user
 	'''
 	return Credential.display_credentials(user_name)
-
-    def copy_credential(site_name):
+	
+def copy_credential(site_name):
 	'''
 	Function to copy a credentials details to the clipboard
 	'''
 	return Credential.copy_credential(site_name)
 
-    def main():
+
+def main():
     while True:
-        print("Welcome to Password locker")
-
-        rint('\n')
-        print("pick a code to navigate through:to create new user use 'new':To login into your account 'login' or 'x' to exit")
-
-        '''
-        creating a new user
-
-        '''
-
-        if short_code == 'new':
+        print("Thank you for joining password locker")
+        print('\n')
+        print("pick a code to navigate through:to create new user use 'nu':To login into your account 'lg' or 'ex' to exit")
+        short_code = input().lower()
+        print('\n')
+        if short_code == 'nu':
             print('create username')
             created_user_name = input()
-
-             print('create password')
+            print('create password')
             created_user_password = input()
+            print('confirm password')
+            confirm_password = input()
+            while confirm_password != created_user_password:
+                print('invalid password did not match!')
+                print('enter your password')
+                created_user_password = input()
+                print('confirm your password')
+                confirm_password = input()
+            else:
+                print(f"congratulations{created_user_name}!account creation successful")
+                print('\n')
+                print("proceed to login")
+                print("username")
+                entered_username = input()
+                print("your password")
+                entered_password = input()
+            while entered_username != created_user_name or entered_password != created_user_password:
+                print("invalid username or password")
+                print("username")
+                entered_username = input()
+                print("your password")
+                entered_password = input()
+            else:
+                print(f"welcome: {entered_username} to your account")
+                print("\n")
+        elif short_code == 'lg':
+            print("welcome")
+            print("Enter user name")
+            default_user_name = input()
+            print("Enter password")
+            default_user_password =input()
+            print('\n')
+            while default_user_name != 'testuser' or default_user_password != '09876':
+                print("wrong userName or password.Username 'testuser' and password '09876'")
+                print("Enter user name")
+                default_user_name = input()
+                print("Enter password")
+                default_user_password = input()
+                print("\n")
+            else:
+                print("login success")
+                print('\n')
+                print('\n')
+        elif short_code == 'ex':
+            break
+        else:
+            print("Enter valid code to continue")
+if __name__ == '__main__':
+    main()
+            
--- user.py
+++ user.py
@@ -1,19 +1,22 @@
 class User:
-    '''
-    Class to Create user accounts and save their information
-    '''
-    users_list = []
-    def __init__(self, first_name,last_name):
-        '''
-        Method to define the properties for each user object will hold.
-        '''
-        # instance variables
-        self.first_name = first_name
+	'''
+	Class to create user accounts and save their information
+	'''
+	# Class Variables
+	# global users_list
+	users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
+
+		# instance variables
+		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
-    def save_user(self):
-        '''
-        Function to save a newly created user instance
-        '''
-        User.users_list.append(self):
-        
+
+	def save_user(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		User.users_list.append(self)
