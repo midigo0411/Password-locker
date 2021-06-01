9179b1d6ab699eee683f518da42df757ed14c010
midigo0411 <midigo041@gmail.com>
2021-06-01 23:36:14 +0300

    function to create new user account

--- password-locker.py
+++ password-locker.py
@@ -1,3 +1,11 @@
 import pyperclip
 from credentials import Credential
 from user import User
+
+
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname,lname,password)
+	return new_user
